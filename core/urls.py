2e433b1ad74d530f6e06e7fa01e54ab618a244fd
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2025-07-26 13:35:34 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -9,9 +9,9 @@ from koguilator import views
 urlpatterns = [
 
     path("admin/", admin.site.urls),
-    path('', views.calculadora_view, name='calculadora'),
-    path("index/", views.login_registro_view, name='index'),
-    path('logout/', LogoutView.as_view(next_page='index'), name='logout'),
+    path("", views.calculadora_view, name="calculadora"),
+    path("index/", views.login_registro_view, name="index"),
+    path("logout/", LogoutView.as_view(next_page="index"), name="logout"),
     
 ] 
 
